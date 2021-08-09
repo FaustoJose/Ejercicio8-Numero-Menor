293313dcaaf3570008e51abe63d89cade7dfec04
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:38:52 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio8 Numero Menor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio8_Numero_Menor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Resultado = NumeroMenor(1, 2);
+            Console.WriteLine("El numero menor es:"+Resultado);
+        }
+        public static int NumeroMenor(int n1,int n2)
+        {
+            if (n1<n2)
+            {
+                return n1;
+            }
+            else
+            {
+                return n2;
+            }
+        }
+    }
+}
